a3bd899d8d60a28a37822a2a850abacb09a26b27
josephvong <m15914375662@163.com>
2016-11-27 16:56:25 +0800

    20161127_1_路由器+移动端边框1px处理

---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+     this is a ratings
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'ratings'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+ 
+</style>
